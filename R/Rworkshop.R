fd9857fd88c26452fd5c8e16b9e6daf3bf780f68
Sjoerd Huisman <s.m.h.huisman@fsw.leidenuniv.nl>
2023-06-22 12:44:24 +0000

    just working

--- R/Rworkshop.R
+++ R/Rworkshop.R
@@ -9,4 +9,28 @@ usethis::use_git_config(
 
 usethis::use_git()
 
+# Create a token ("login")
+usethis::create_github_token(description = "Token for Repro Workshop 2023 Test")
+
+# Activate scope write:packages <- do this
+
+# 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 <- token for now (set to 7 days)
+
+# activate the token
+gitcreds::gitcreds_set() 
+
+# check if it worked
+usethis::gh_token_help()
+
+# to actually connect to github
+usethis::use_github()
+# usethis::use_github(private = TRUE) make it into a private project instead
+# Note: the name of the github project is automatically, and irreversibly (!)
+# the name of your R project folder.
+# So give it an informative name (and not "Project")
+
+
+
+
+
 
