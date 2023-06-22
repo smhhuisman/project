4173f201e019ce8b67fcac3f2004064b31c7180e
Sjoerd Huisman <s.m.h.huisman@fsw.leidenuniv.nl>
2023-06-22 12:40:03 +0000

    Initial commit

---
+++ R/Rworkshop.R
@@ -0,0 +1,12 @@
+
+# use a function without loading the package:
+# package::function
+usethis::use_git_config(
+  user.name = "Sjoerd Huisman", # <-- change to your name
+  user.email = "s.m.h.huisman@fsw.leidenuniv.nl", # <-- and your email
+  init.defaultBranch = "main") # <-- not necessary but kinder than 'master'
+
+
+usethis::use_git()
+
+
